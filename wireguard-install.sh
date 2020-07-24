d159b1b52f163cb1c440ec509ff60b1e9e4fee9b
PedgeTips <68688872+PedgeTips@users.noreply.github.com>
2020-07-24 10:09:24 +0100

    Update wireguard-install.sh

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -199,6 +199,12 @@ fi
 
 # Enable routing on the server
 echo "net.ipv4.ip_forward = 1
+ufw allow 1194
+ufw allow openSSH
+ufw disable
+ufw enable
+ufw reload
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o ens4 -j MASQUERADE
 net.ipv6.conf.all.forwarding = 1" >/etc/sysctl.d/wg.conf
 
 sysctl --system
